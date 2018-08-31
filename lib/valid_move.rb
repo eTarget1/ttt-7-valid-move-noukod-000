bb38c5cee6d16cbfa1282feb7e509ebec2f318db
clifford <clifford@noukod.com>
2018-08-31 15:38:59 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -5,16 +5,14 @@ def valid_move?(board, index)
 if array[ind] == " " || array[ind] == ""  || array[ind] == nil
 return false
 else
-
 return true
-
 end
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
 
 def on_board?(num)
-  if num.between?(0,8) = true
+  if num.between?(0,8) == true
     return true
   else
     return
